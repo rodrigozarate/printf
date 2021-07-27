735e864e0273b36eec00dbf37961412f198dd8e1
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-27 11:55:42 -0500

    [ADD] Flavors

--- _printf.c
+++ _printf.c
@@ -14,5 +14,20 @@
 
 int _printf(const char *format, ...)
 {
-
+/*  no format */
+if (!format || !format[0])
+		return (-1);
+/* flavor */
+w_print t[] = {
+	{'d', print_int},
+	{'i', print_int},
+	{'u', print_unsigned_int},
+	{'o', print_octal},
+	{'x', print_hex},
+	{'X', print_hex},
+	{'c', print_char},
+	{'s', print_string},
+	{'p', print_pointer},
+	{'r', print_rev},
+	};
 }
