c3024cf8cf5db5b760b2811c7ffff2ec35bebb24
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-28 18:17:02 -0500

    [UPDATE] print integer function

--- holberton.h
+++ holberton.h
@@ -1,9 +1,21 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#define BUFFER_SIZE 1024
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
+
+typedef struct arg_s
+{
+	char op;
+	char* (*f)();
+} op_t;
+
+char* (*get_op_func(char *s))(va_list);
+char *print_int(va_list theList);
 
 #endif /* HOLBERTON_H */
 
--- print_int.c
+++ print_int.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /*
 * print_int.c - Function when i or d
 * Author: Rodrigo Zarate & Carlos Matallana
@@ -9,60 +11,42 @@
 * @theList: va_arg
 * Return: String to print
 */
-
 char *print_int(va_list theList)
 {
-char *whattosave;
-int n;
-int i = 0, j = 0;
-char muffer[10]; /* for the muuuus */
-unsigned int mu;
+	char *whattosave;
+	int n;
+	int i = 0, j = 0;
+	char muffer[10]; /* for the muuuus */
+	unsigned int mu;
 
-/* look for null */
-if(!theList)
-	return (NULL);
-/* case zero */
-n = va_arg(theList, int);
+	if (!theList)
+		return (NULL);
+	n = va_arg(theList, int);
 	if (n == 0)
 	{
 		j = 1;
-		whattosave = malloc(sizeof(char) * 2);
-		whattosave[0] = '0';
+		whattosave = malloc(sizeof(char) * 2);a
+				whattosave[0] = '0';
 	}
 	else
 	{
-	/* minor than zero */
 		if (n < 0)
-		/* save conversion */
 			mu = -n;
 		else
 			mu = n;
 
-		/* get the numbers char by char */
-		/* positive part */
 		while (mu)
 		{
-			/* modulo operand and buffer++ */
-			/* put the numbers one by one in buffer on reverse */
 			muffer[i++] = (mu % 10) + '0';
-			/* divide by ten */
 			mu = mu / 10;
 		}
 		if (n < 0)
 			muffer[i++] = '-';
-			/* put minus sing at the end if negative */
 		whattosave = malloc(sizeof(char) * i);
-		/* i-- end of string */
 		i--;
-		/* reorder and save on var to send to func */
 		while (i >= 0)
-		{
 			whattosave[j++] = muffer[i--];
-		}
-
 	}
-/* put end of str */
-whattosave[j] = '\0';
-/* send to caller */
-return (whattosave);
+	whattosave[j] = '\0';
+	return (whattosave);
 }
