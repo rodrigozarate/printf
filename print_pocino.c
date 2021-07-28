96e24591d0d78af4710546f8534e16767eab1331
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-28 16:05:39 -0500

    [CREATE]

---
+++ print_pocino.c
@@ -0,0 +1,11 @@
+/*
+* print_porcino.c - Function when %
+* Author: Rodrigo Zarate & Carlos Matallana
+* Date: July 28, 2021
+*/
+
+/**
+* print_porcino - Print formated
+* @theList: va_arg
+* Return: String to print
+*/
