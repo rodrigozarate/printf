43e49a33dce966d657b39f4b3b51aa263891e07c
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-28 19:30:02 -0500

    [FIX] Remove comments

--- get_op_func.c
+++ get_op_func.c
@@ -4,18 +4,18 @@
 char *(*get_op_func(char *s))(va_list)
 {
 	op_t ops[] = {
-			/*{'c', print_char},
-			{'s', print_str},*/
+			{'c', print_char},
+			{'s', print_str},
 			{'d',  print_int},
 			{'i',  print_int},
-			/*{'%', print_char},*/
+			{'%', print_char},
 			{'\0', NULL}
 	};
 	unsigned int i = 0;
 
 	while (ops[i].op)
 	{
-		if (ops[i].op == *s) /* no puedo comparar un caracter con puntero */
+		if (ops[i].op == *s)
 			return (ops[i].f);
 		i++;
 	}
